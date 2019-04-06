064a4534387db50c4be0a552d8234bc6037c59ba
nahara1 <narr.melo@gmail.com>
2019-04-06 19:14:12 -0400

    Update app.html

--- app.js
+++ app.js
@@ -430,299 +430,61 @@ function goForward() {
   window.history.forward()
 }
 
+// get user's geolocation
 
-  //messaging starts
+ipLookUp(function(getAddress()) {
+        document.getElementById('addr').innerHTML = ips.join(',');
+    });
+function ipLookUp () {
+  $.ajax('http://ip-api.com/json')
+  .then(
+      function success(response) {
+          console.log('User\'s Location Data is ', response);
+          console.log('User\'s Country', response.country);
+          getAddress(response.lat, response.lon)
+},
 
-  // chrome.gcm.register(['22916148354'], function(rId) {
-  //   console.log('registered: ');
-  //   console.log(rId);
-  //   registerCallback(rId);
-  // })
+      function fail(data, status) {
+          console.log('Request failed.  Returned status of',
+                      status);
+      }
+  );
+}
 
+function getAddress (latitude, longitude) {
+  $.ajax('https://maps.googleapis.com/maps/api/geocode/json?' +
+          'latlng=' + latitude + ',' + longitude + '&key=' +
+          GOOGLE_MAP_KEY)
+  .then(
+    function success (response) {
+      console.log('User\'s Address Data is ', response)
+    },
+    function fail (status) {
+      console.log('Request failed.  Returned status of',
+                  status)
+    }
+   )
+}
 
-  // chrome.runtime.onStartup.addListener(function() {
-  //   console.log("meow");
-  //   chrome.storage.local.get("registered", function(result) {
-  //     // If already registered, bail out.
-  //     if (result["registered"])
-  //       console.log("already registered");
-  //       return;
-
-  //     // Up to 100 senders are allowed.
-  //     var senderIds = ["22916148354"];
-  //     chrome.gcm.register(senderIds, registerCallback);
-  //     console.log("registed senderIds");
-  //     console.log("about to send message");
-  //     sendMessage();
-  //   });
-  // });
-
-
-// if (window == top) {
-//   chrome.extension.onMessage.addListener(function(req, sender, sendMessage) {
-//     console.log("Got request");
-//     doStuff();
-//     // sendMessage('Done!');
-//   });
-// }
-
-// function doStuff() {
-//   console.log("meow");
-// }
-
-
-// var registerCallback = function(registrationId) {
-
-//   if (chrome.runtime.lastError) {
-//     console.log("error registering!");
-//     // When the registration fails, handle the error and retry the
-//     // registration later.
-//     return;
-//   }
-
-//   console.log('no error registering:');
-//   console.log(registrationId);
-
-//   // Send the registration token to your application server.
-//   sendRegistrationId(function(succeed) {
-//     // Once the registration token is received by your server,
-//     // set the flag such that register will not be invoked
-//     // next time when the app starts up.
-//     if (succeed) {
-//       chrome.storage.local.set({registered: true});
-
-//     }
-
-//   });
-
-
-//   console.log("about to send message");
-//   sendMessage();
-
-// }
-
-// }
-
-
-// function sendRegistrationId(callback) {
-//   // Send the registration token to your application server
-//   // in a secure way.
-// }
-
-
-// chrome.runtime.onStartup.addListener(function() {
-//   chrome.storage.local.get("registered", function(result) {
-//     // If already registered, bail out.
-//     if (result["registered"])
-//       return;
-
-
-//     // Up to 100 senders are allowed.
-//     var senderIds = ["499742986420"];
-//     chrome.gcm.register(senderIds, registerCallback);
-//   });
-// });
-
-
-//                     //RECEIVING
-
-
-
-                    //RECEIVING
-
-
-// chrome.gcm.onMessage.addListener(function(message) {
-//   console.log("received message");
-//   // A message is an object with a data property that
-//   // consists of key-value pairs.
-// });
-
-
-
-
-//                     //SENDING
-
-
-// // Substitute your own sender ID here. This is the project
-// // number you got from the Google Developers Console.
-// var senderId = "499742986420";
-
-// // Make the message ID unique across the lifetime of your app.
-// // One way to achieve this is to use the auto-increment counter
-// // that is persisted to local storage.
-
-// // Message ID is saved to and restored from local storage.
-// var messageId = 0;
-// chrome.storage.local.get("messageId", function(result) {
-//   if (chrome.runtime.lastError)
-//     return;
-//   messageId = parseInt(result["messageId"]);
-//   if (isNaN(messageId))
-//     messageId = 0;
-// });
-
-// // Sets up an event listener for send error.
-// chrome.gcm.onSendError.addListener(sendError);
-
-// // Returns a new ID to identify the message.
-// function getMessageId() {
-//   messageId++;
-//   chrome.storage.local.set({messageId: messageId});
-//   return messageId.toString();
-// }
-
-// function sendMessage() {
-//   var message = {
-//     messageId: getMessageId(),
-//     destinationId: senderId + "@gcm.googleapis.com",
-//     timeToLive: 86400,    // 1 day
-//     data: {
-//       "key1": "value1",
-//       "key2": "value2"
-//     }
-//   };
-//   chrome.gcm.send(message, function(messageId) {
-//     if (chrome.runtime.lastError) {
-//       // Some error occurred. Fail gracefully or try to send
-//       // again.
-//       console.log("error in sending message");
-//       return;
-//     }
-//     console.log("message accepted for delivery");
-
-//     // The message has been accepted for delivery. If the message
-//     // can not reach the destination, onSendError event will be
-//     // fired.
-//   });
-// }
-
-// function sendError(error) {
-//   console.log("Message " + error.messageId +
-//       " failed to be sent: " + error.errorMessage);
-// }
-
-
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////
-/**
- * Original Code from the Extension Tutorial that isn't useful but is still a good reference for now.
- */
-///////////////////////////////////////////////////////////////////////////////////////////////////////
-
-/**
- * Get the current URL.
- *
- * @param {function(string)} callback - called when the URL of the current tab
- *   is found.
- */
-// function getCurrentTabUrl(callback) {
-//   // Query filter to be passed to chrome.tabs.query - see
-//   // https://developer.chrome.com/extensions/tabs#method-query
-//   var queryInfo = {
-//     active: true,
-//     currentWindow: true
-//   };
-
-//   chrome.tabs.query(queryInfo, function(tabs) {
-//     // chrome.tabs.query invokes the callback with a list of tabs that match the
-//     // query. When the popup is opened, there is certainly a window and at least
-//     // one tab, so we can safely assume that |tabs| is a non-empty array.
-//     // A window can only have one active tab at a time, so the array consists of
-//     // exactly one tab.
-//     console.log(tabs);
-//     var tab = tabs[0];
-
-//     // A tab is a plain object that provides information about the tab.
-//     // See https://developer.chrome.com/extensions/tabs#type-Tab
-//     var url = tab.url;
-
-//     // tab.url is only available if the "activeTab" permission is declared.
-//     // If you want to see the URL of other tabs (e.g. after removing active:true
-//     // from |queryInfo|), then the "tabs" permission is required to see their
-//     // "url" properties.
-//     console.assert(typeof url == 'string', 'tab.url should be a string');
-
-//     callback(url);
-//   });
-
-//   // Most methods of the Chrome extension APIs are asynchronous. This means that
-//   // you CANNOT do something like this:
-//   //
-//   // var url;
-//   // chrome.tabs.query(queryInfo, function(tabs) {
-//   //   url = tabs[0].url;
-//   // });
-//   // alert(url); // Shows "undefined", because chrome.tabs.query is async.
-// }
-
-// /**
-//  * @param {string} searchTerm - Search term for Google Image search.
-//  * @param {function(string,number,number)} callback - Called when an image has
-//  *   been found. The callback gets the URL, width and height of the image.
-//  * @param {function(string)} errorCallback - Called when the image is not found.
-//  *   The callback gets a string that describes the failure reason.
-//  */
-// function getImageUrl(searchTerm, callback, errorCallback) {
-//   // Google image search - 100 searches per day.
-//   // https://developers.google.com/image-search/
-//   var searchUrl = 'https://ajax.googleapis.com/ajax/services/search/images' +
-//     '?v=1.0&q=' + encodeURIComponent(searchTerm);
-//   var x = new XMLHttpRequest();
-//   x.open('GET', searchUrl);
-//   // The Google image search API responds with JSON, so let Chrome parse it.
-//   x.responseType = 'json';
-//   x.onload = function() {
-//     // Parse and process the response from Google Image Search.
-//     var response = x.response;
-//     console.log(response);
-//     if (!response || !response.responseData || !response.responseData.results ||
-//         response.responseData.results.length === 0) {
-//       errorCallback('No response from Google Image search!');
-//       return;
-//     }
-//     var firstResult = response.responseData.results[0];
-//     // Take the thumbnail instead of the full image to get an approximately
-//     // consistent image size.
-//     var imageUrl = firstResult.tbUrl;
-//     var width = parseInt(firstResult.tbWidth);
-//     var height = parseInt(firstResult.tbHeight);
-//     console.assert(
-//         typeof imageUrl == 'string' && !isNaN(width) && !isNaN(height),
-//         'Unexpected respose from the Google Image Search API!');
-//     callback(imageUrl, width, height);
-//   };
-//   x.onerror = function() {
-//     errorCallback('Network error.');
-//   };
-//   x.send();
-// }
-
-// function renderStatus(statusText) {
-//   document.getElementById('status').textContent = statusText;
-// }
-
-// document.addEventListener('DOMContentLoaded', function() {
-//   getCurrentTabUrl(function(url) {
-//     // Put the image URL in Google search.
-//     renderStatus('Performing Google Image search for ' + url);
-
-//     getImageUrl(url, function(imageUrl, width, height) {
-
-//       renderStatus('Search term: ' + url + '\n' +
-//           'Google image search result: ' + imageUrl);
-//       var imageResult = document.getElementById('image-result');
-//       // Explicitly set the width/height to minimize the number of reflows. For
-//       // a single image, this does not matter, but if you're going to embed
-//       // multiple external images in your page, then the absence of width/height
-//       // attributes causes the popup to resize multiple times.
-//       imageResult.width = width;
-//       imageResult.height = height;
-//       imageResult.src = imageUrl;
-//       imageResult.hidden = false;
-
-//     }, function(errorMessage) {
-//       renderStatus('Cannot display image. ' + errorMessage);
-//     });
-//   });
-// });
+if ("geolocation" in navigator) {
+  // check if geolocation is supported/enabled on current browser
+  navigator.geolocation.getCurrentPosition(
+   function success(position) {
+     // for when getting location is a success
+     console.log('latitude', position.coords.latitude,
+                 'longitude', position.coords.longitude);
+     getAddress(position.coords.latitude,
+                position.coords.longitude)
+   },
+  function error(error_message) {
+    // for when getting location results in an error
+    console.error('An error has occured while retrieving' +
+                  'location', error_message)
+    ipLookUp()
+  });
+} else {
+  // geolocation is not supported
+  // get your location some other way
+  console.log('geolocation is not enabled on this browser')
+  ipLookUp()
+}
